1f958a078bc1984d20bc7c0c945e6fbd5a96709a
Carlos Icaza <icazacarlos@gmail.com>
2023-04-10 09:16:44 -0500

    Fix: sl-abr-2023

--- src/main/java/com/javabycode/springboot/MyWebApplication.java
+++ src/main/java/com/javabycode/springboot/MyWebApplication.java
@@ -2,16 +2,18 @@ package com.javabycode.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+//import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class MyWebApplication extends SpringBootServletInitializer{
+public class MyWebApplication {
+    /*
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(MyWebApplication.class);
     }
+    */
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(MyWebApplication.class, args);
